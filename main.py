39279bab52ebfc721c0669fedfe0d63105ef195d
Tavorn <tavornsaewang@gmail.com>
2022-03-10 22:43:03 +0700

    create colors list

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
-import random
-from turtle import Turtle, Screen
+import colorgram
 
-tim = Turtle()
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
-tim.shape("turtle")
-tim.pensize(20)
-directions = [0, 90, 180, 270]
-tim.speed("fastest")
-screen = Screen()
-screen.colormode(255)
+print(rgb_colors)
 
-
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    colors = (r, g, b)
-    return colors
-
-
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
-
+my_colors = [
+    (212, 149, 95), (215, 80, 62), (47, 94, 142), (231, 218, 92), (148, 66, 91), (22, 27, 40), (155, 73, 60),
+    (122, 167, 195), (40, 22, 29), (39, 19, 15), (209, 70, 89), (192, 140, 159), (39, 131, 91), (125, 179, 141),
+    (75, 164, 96), (229, 169, 183), (15, 31, 22), (51, 55, 102), (233, 220, 12), (159, 177, 54), (99, 44, 63),
+    (35, 164, 196), (234, 171, 162), (105, 44, 39), (164, 209, 187), (151, 206, 220)
+]
 
 """
+# import random
+# import turtle as t
+# 
+# t.colormode(255)
+# t.speed("fastest")
+# 
+# 
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color = (r, g, b)
+#     return color
+# 
+# 
+# def draw_circle(size_of_gap):
+#     for _ in range(int(360 / size_of_gap)):
+#         t.color(random_color())
+#         t.circle(180)
+#         t.setheading(t.heading() + size_of_gap)
+# 
+# 
+# draw_circle(4)
+# tim.pensize(20)
+# directions = [0, 90, 180, 270]
+# tim.speed("fastest")
+# for _ in range(100):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
 # def draw_shape(num_sides):
 #     for _ in range(num_sides):
 #         angle = 360 / num_sides
@@ -36,7 +59,5 @@ for _ in range(200):
 # for shape_side_n in range(3, 11):
 #     tim.color(random.choice(colors))
 #     draw_shape(shape_side_n)
+# t.exitonclick()
 """
-
-
-screen.exitonclick()
