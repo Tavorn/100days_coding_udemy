56b88b24aa6321e1f465021d87847cfcc9aa6cb2
Tavorn <tavornsaewang@gmail.com>
2022-03-10 19:04:27 +0700

    colors

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+import random
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
+tim = Turtle()
 
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tim.shape("turtle")
+tim.pensize(20)
+directions = [0, 90, 180, 270]
+tim.speed("fastest")
 screen = Screen()
+screen.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
+
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+
+"""
+# def draw_shape(num_sides):
+#     for _ in range(num_sides):
+#         angle = 360 / num_sides
+#         tim.forward(100)
+#         tim.right(angle)
+# 
+# 
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
+"""
+
+
 screen.exitonclick()
-screen.screensize(300, 300)
