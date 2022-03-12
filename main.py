363a2aff5bc172418175c7bab1244b406dff91d8
Tavorn <tavornsaewang@gmail.com>
2022-03-12 22:08:48 +0700

    day 22 - 199

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+
+screen.exitonclick()
+
+"""
+# width = 20
+# height = 100
+# x_pos = 350
+# y_pos = 0
+
+move up
+move down
+"""
