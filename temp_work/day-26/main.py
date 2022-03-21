5de3585902e3a824683c84e762f34fd860e3336b
Tavorn <tavornsaewang@gmail.com>
2022-03-21 21:35:09 +0700

    day 26-243

--- temp_work/day-26/main.py
+++ temp_work/day-26/main.py
@@ -1,4 +1,15 @@
-
+"""
+# import random
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# students_scores = {student: random.randint(1,100) for student in names}
+# print(students_scores)
+# passed_students = {student: score for (student, score) in students_scores.items() if score >= 60}
+# print(passed_students)
+#
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# result = {student: len(student) for student in sentence.split()}
+# print(result)
+"""
 
 """
 # new_dict = {new_key:new_value for item in list}
