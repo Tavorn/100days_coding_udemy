0e8c539f32c02a82ed85c74f748a53bc9c224db2
Tavorn <tavornsaewang@gmail.com>
2022-03-21 19:04:43 +0700

    day 26-236

---
+++ temp_work/day-26/main.py
@@ -0,0 +1,42 @@
+
+"""
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above
+#
+# #Write your 1 line code 👇 below:
+#
+# result = [num for num in numbers if num % 2 == 0]
+#
+# #Write your code 👆 above:
+#
+# print(result)
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above 👆
+#
+# #Write your 1 line code 👇 below:
+#
+# squared_numbers = [num * num for num in numbers]
+#
+# #Write your code 👆 above:
+#
+# print(squared_numbers)
+
+# numbers = [1, 2, 3]
+# new_list = []
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# 
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+# 
+# # new_list = [new_item for item in list]
+# # new_list = [new_item for item in list if test]
+# new_list = [n + 1 for n in numbers]
+# 
+# short_name = [name for name in names if len(name) < 5]
+# 
+# upper_name = [name.upper() for name in names if len(name) > 5]
+# 
+# print(upper_name)
+"""
