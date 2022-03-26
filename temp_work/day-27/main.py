b23fb6351eee92447af0098cc731b89e2888ae39
Tavorn <tavornsaewang@gmail.com>
2022-03-26 22:40:09 +0700

    day 26-252 Finished

--- temp_work/day-27/main.py
+++ temp_work/day-27/main.py
@@ -1,100 +1,39 @@
 from tkinter import *
 
-# Creating a new window and configurations
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
+
+
 window = Tk()
-window.title("Widget Examples")
-window.minsize(width=500, height=500)
+window.title("Mile to Km Converter")
+window.config(padx=20, pady=20)
 
-# Labels
-label = Label(text="This is old text")
-label.config(text="This is new text")
-label.pack()
+miles_input = Entry(width=7)
+miles_input.insert(END, string="0")
+miles_input.grid(column=1, row=0)
 
+miles_label = Label(text="Miles", font=("Arial", 24, "bold"))
+miles_label.grid(column=2, row=0)
+miles_label.config(padx=10, pady=10)
 
-# Buttons
-def action():
-    print("Do something")
+is_equal_label = Label(text="is equal to", font=("Arial", 24, "bold"))
+is_equal_label.grid(column=0, row=1)
+is_equal_label.config(padx=10, pady=10)
 
+kilometer_result_label = Label(text=0, font=("Arial", 24, "bold"))
+kilometer_result_label.grid(column=1, row=1)
+kilometer_result_label.config(padx=10, pady=10)
 
-# calls action() when pressed
-button = Button(text="Click Me", command=action)
-button.pack()
+kilometer_label = Label(text="Km", font=("Arial", 24, "bold"))
+kilometer_label.grid(column=2, row=1)
+kilometer_label.config(padx=10, pady=10)
 
-# Entries
-entry = Entry(width=30)
-# Add some text to begin with
-entry.insert(END, string="Some text to begin with.")
-# Gets text in entry
-print(entry.get())
-entry.pack()
-
-# Text
-text = Text(height=5, width=30)
-# Puts cursor in textbox.
-text.focus()
-# Adds some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
-# Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
-text.pack()
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
+calculate_button.config(pady=10, padx=10)
 
 
-# Spinbox
-def spinbox_used():
-    # gets the current value in spinbox.
-    print(spinbox.get())
-
-
-spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
-spinbox.pack()
-
-
-# Scale
-# Called with current scale value.
-def scale_used(value):
-    print(value)
-
-
-scale = Scale(from_=0, to=100, command=scale_used)
-scale.pack()
-
-
-# Checkbutton
-def checkbutton_used():
-    # Prints 1 if On button checked, otherwise 0.
-    print(checked_state.get())
-
-
-# variable to hold on to checked state, 0 is off, 1 is on.
-checked_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
-checked_state.get()
-checkbutton.pack()
-
-
-# Radiobutton
-def radio_used():
-    print(radio_state.get())
-
-# Variable to hold on to which radio button value is checked.
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
-radiobutton1.pack()
-radiobutton2.pack()
-
-
-# Listbox
-def listbox_used(event):
-    # Gets current selection from listbox
-    print(listbox.get(listbox.curselection()))
-
-
-listbox = Listbox(height=4)
-fruits = ["Apple", "Pear", "Orange", "Banana"]
-for item in fruits:
-    listbox.insert(fruits.index(item), item)
-listbox.bind("<<ListboxSelect>>", listbox_used)
-listbox.pack()
 window.mainloop()
-
