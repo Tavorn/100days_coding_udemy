2af404ec261399ac6464faa3693b46fddabcc1ee
Tavorn <tavornsaewang@gmail.com>
2022-03-28 18:26:22 +0700

    day 29 - Finished

--- temp_work/day-29/main.py
+++ temp_work/day-29/main.py
@@ -1,14 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data:
+                data.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_png = PhotoImage(file="logo.png")
@@ -18,25 +57,78 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
-user_label = Label(text="Email/Username:")
-user_label.grid(column=0, row=2)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
 
-user_input = Entry(width=35)
-user_input.grid(column=1, row=2, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "example@example.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-password_input = Entry(width=21)
-password_input.grid(column=1, row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
 
-button = Button(text="Generate Password")
-button.grid(column=2, row=3)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(column=2, row=3)
 
-button = Button(text="Add", width=36)
-button.grid(column=1, row=4, columnspan=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
+
+"""
+# #Password Generator Project
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+  'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# 
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+# 
+# #Eazy Level
+# # password = ""
+# 
+# # for char in range(1, nr_letters + 1):
+# #   password += random.choice(letters)
+# 
+# # for char in range(1, nr_symbols + 1):
+# #   password += random.choice(symbols)
+# 
+# # for char in range(1, nr_numbers + 1):
+# #   password += random.choice(numbers)
+# 
+# # print(password)
+# 
+# #Hard Level
+# password_list = []
+# 
+# for char in range(1, nr_letters + 1):
+#   password_list.append(random.choice(letters))
+# 
+# for char in range(1, nr_symbols + 1):
+#   password_list += random.choice(symbols)
+# 
+# for char in range(1, nr_numbers + 1):
+#   password_list += random.choice(numbers)
+# 
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+# 
+# password = ""
+# for char in password_list:
+#   password += char
+# 
+# print(f"Your password is: {password}")
+"""
